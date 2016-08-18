cb01e0062093b41bf6d528987153fad20031b2f9
Mattia Primavera <sconqua@gmail.com>
2016-08-18 03:24:45 +0200

    Find command moved to commands.js #37

--- js/commands.js
+++ js/commands.js
@@ -53,4 +53,54 @@ service('CommandsService', function($http){
 
 	    return promise;
 	};//fin ls method
+
+
+
+	this.find = function(){
+        $http.get("/command/", {
+            params: {
+                "command": command,
+                "currentDir": $scope.currentDir,
+            }
+        })
+        .success(function(data, status, headers, config) {
+            if (data["type"] == "1") {
+                console.log("Error: " + data["message"]);
+            } else {
+                /* updating file manager view */
+                data[0] = {
+                    "name": "..",
+                    "size": "0",
+                    "last_modified": "/",
+                    "type": "link"
+                };
+                $scope.files = data;
+
+                /* updating current dir */
+                if ($scope.command !== "find") { /* if ls has a parameter */
+                    console.log("Command: " + $scope.command);
+                    var ls_path = $scope.command.split(" ")[1];
+                    if (ls_path === "..") {
+                        if ($scope.currentDir !== "/") {
+                            $scope.currentDir = $scope.currentDir.split('/');
+                            $scope.currentDir.pop();
+                            $scope.currentDir = $scope.currentDir.join("/");
+                            if (!$scope.currentDir.startsWith('/'))
+                                $scope.currentDir = '/' + $scope.currentDir;
+                        }
+                    } else { /* ls with parameter != .. */
+                        if ($scope.currentDir.endsWith('/'))
+                            $scope.currentDir += ls_path;
+                        else
+                            $scope.currentDir += '/' + ls_path;
+                    }
+                }
+                console.log("currentDir: " + $scope.currentDir);
+
+            }
+        })
+        .error(function(data, status, headers, config) {
+            console.log("-ERR Communication With Server");
+        });
+	};// end of find method
 });
